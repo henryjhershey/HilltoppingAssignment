21dcef6a85d49ac89c8ad0aa5ab4e58a5889ec95
Henry Hershey <34039859+henryjhershey@users.noreply.github.com>
2021-09-12 12:50:08 -0500

    Update NewPop.R

--- source/NewPop.R
+++ source/NewPop.R
@@ -1,8 +1,8 @@
 NewPop = function(nindvs,landscape,originwidth){
-  pop = matrix(nrow=nindvs,ncol=2)
-  xstart <- round(runif(1,1,landscape-originwidth))
-  ystart <- round(runif(1,1,landscape-originwidth))
-  pop[,1] = sample(xstart:(xstart+originwidth),nindvs,replace=T)
-  pop[,2] = sample(ystart:(ystart+originwidth),nindvs,replace=T)
+  pop = matrix(nrow=starts$nindvs,ncol=2)
+  xstart <- round(runif(1,1,starts$landscape-starts$originwidth))
+  ystart <- round(runif(1,1,starts$landscape-starts$originwidth))
+  pop[,1] = sample(xstart:(xstart+starts$originwidth),starts$nindvs,replace=T)
+  pop[,2] = sample(ystart:(ystart+starts$originwidth),starts$nindvs,replace=T)
   return(pop)
 }
